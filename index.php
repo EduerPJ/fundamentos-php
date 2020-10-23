0cd2131ac3af3891fe10885bfa3fc296174c56a2
EduerPJ <eduerpj@gmail.com>
2020-10-23 10:32:24 -0500

    inicio repaso de fundamentos php | autoload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// show error
+ini_set('display_errors', 1);
+ini_set('display_starup_error', 1);
+error_reporting(E_ALL);
+
+// Sistema de autocarga de clases instalado de composer
+require __DIR__ . '/vendor/autoload.php';
+
+
+$text = 'HOLA MUNDO';
+
+// carpeta>clase>método
+
+echo upper('hola');
+
+echo "<br>";
+
+echo lower($text);
